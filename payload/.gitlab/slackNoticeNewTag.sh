0b1564a5fa2467e4c8917b91c4d7df2aafae7b82
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2021-10-06 17:25:34 +0200

    [FEATURE] add new deploy step: slack notice for tags

---
+++ payload/.gitlab/slackNoticeNewTag.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+lastCommit="$(git log -1 --pretty=%B)"
+message="payload={
+    \"attachments\": [
+        {
+            \"fallback\": \"${CI_PROJECT_NAME} has new tag ${CI_COMMIT_TAG}!\",
+            \"color\": \"good\",
+            \"author_name\": \"${CI_PROJECT_NAME}\",
+            \"author_icon\": \"https://gitlab.com/gitlab-com/gitlab-artwork/raw/master/logo/logo-square.png\",
+            \"author_link\": \"${CI_PROJECT_URL}\",
+            \"title\": \"Successfully released tag ${CI_COMMIT_TAG}\",
+            \"title_link\": \"${CI_PIPELINE_URL}\",
+            \"text\": \"${lastCommit}\",
+            \"footer\": \"${GIT_AUTHOR_NAME}\",
+        }
+    ]
+}";
+
+curl -X POST --data-urlencode "${message}" ${SLACK_HOOK}
